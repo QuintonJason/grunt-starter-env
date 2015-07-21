a20be1f6ce512790d98dff9bc78ada67db37d32b
QuintonJason <quinton.jason@gmail.com>
2015-07-21 16:58:58 -0500

    svgstore svg options added

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,7 +47,14 @@ module.exports = function(grunt){
 			    	indent_size : 4
 			    },
 				svg: { 
+		        	version: '1.1',
+		        	display: 'none',
+		        	width: '0',
+		        	height: '0',
+		        	/* style: 'display:none;', */
 		        	xmlns: 'http://www.w3.org/2000/svg',
+		        	'xmlns:xlink': 'http://www.w3.org/1999/xlink'
+
 		      	}
 		    },
 		    default: {
